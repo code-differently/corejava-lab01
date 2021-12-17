09206b6759fd76f32c795c570fb3c5895d3a8a05
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-12-17 11:49:56 -0500

    updates

--- src/main/java/com/codedifferently/labs/lab01/a_scanner_conditional/activities/Activity_A03_SwitchCalculator.java
+++
@@ -1,6 +0,0 @@
-package com.codedifferently.labs.lab01.a_scanner_conditional.activities;
-
-public class Activity_A03_SwitchCalculator {
-    //Todo: create a main method
-    //Todo: Rewrite the code in Example04_Calculator to use a switch statement
-}
